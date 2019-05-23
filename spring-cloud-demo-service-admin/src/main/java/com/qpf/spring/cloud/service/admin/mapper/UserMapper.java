9b335cd380177eea465e73e3efcf03d9cfc0b204
shadaileng <qpf0510@qq.com>
2019-05-23 17:20:49 +0800

    spring-cloud-demo-service-admin add data access(mybatis)

---
+++ spring-cloud-demo-service-admin/src/main/java/com/qpf/spring/cloud/service/admin/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.qpf.spring.cloud.service.admin.mapper;
+
+import com.qpf.spring.cloud.service.admin.entity.User;
+import com.qpf.spring.cloud.utils.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
