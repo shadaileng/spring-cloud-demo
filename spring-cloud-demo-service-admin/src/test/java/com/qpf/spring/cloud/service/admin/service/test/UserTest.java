4d0bd1013831772ac826fa5684e2a39d806d18db
shadaileng <qpf0510@qq.com>
2019-05-27 17:32:06 +0800

    camelToUnderline

--- spring-cloud-demo-service-admin/src/test/java/com/qpf/spring/cloud/service/admin/service/test/UserTest.java
+++ spring-cloud-demo-service-admin/src/test/java/com/qpf/spring/cloud/service/admin/service/test/UserTest.java
@@ -10,6 +10,7 @@ public class UserTest {
     public void property() {
         StringBuffer a = new StringBuffer();
         StringBuffer b = new StringBuffer();
+        StringBuffer c = new StringBuffer();
         for (Field field : User.class.getDeclaredFields()) {
             String name = field.getName();
             if (a.length() > 0) {
@@ -23,9 +24,30 @@ public class UserTest {
                 b.append("#{");
             }
             b.append(name);
+
+
+            if (c.length() > 0) {
+                c.append(", ");
+            }
+            c.append(camelToUnderline(name));
         }
         b.append("}");
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
+    }
+
+    private String camelToUnderline(String param) {
+        StringBuffer line = new StringBuffer();
+
+        for (char c: param.toCharArray()) {
+            if (Character.isUpperCase(c) || Character.isDigit(c)) {
+                line.append("_");
+                c = Character.toLowerCase(c);
+            }
+            line.append(c);
+        }
+
+        return line.toString();
     }
 }
