5cdfa8478ac5b220c0a2823fe403d63bf2d7aa0b
shadaileng <qpf0510@qq.com>
2019-06-22 17:20:29 +0800

    web-admin

--- spring-cloud-demo-web-admin-feign/src/main/java/com/qpf/spring/cloud/demo/web/admin/feign/controller/AdminController.java
+++ spring-cloud-demo-web-admin-feign/src/main/java/com/qpf/spring/cloud/demo/web/admin/feign/controller/AdminController.java
@@ -22,6 +22,6 @@ public class AdminController {
             @RequestParam(required = false, defaultValue = "0") Integer start,
             @RequestParam(required = false, defaultValue = "2") Integer length) {
 
-        return adminService.page(start, length);
+        return null;
     }
 }
